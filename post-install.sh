f4e2bbc65542ff5657801ec3ea0c567be9a67e19
James Wright <jamesw@gmail.com>
2025-06-12 11:34:46 -0500

    Add basic post-install.sh

---
+++ post-install.sh
@@ -0,0 +1,6 @@
+# REMEMBER TO `chmod +x` this file if it is new
+
+# install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+/bin/bash -c "$(brew install tig)"
