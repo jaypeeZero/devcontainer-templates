49f31ccd52a176164472e41bb19698987094bd34
James Wright <jamesw@gmail.com>
2025-06-12 11:41:10 -0500

    Add more brew stuff

--- post-install.sh
+++ post-install.sh
@@ -3,4 +3,9 @@
 # install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo >> /home/vscode/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/vscode/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# install tig (or anything with Homebrew)
 /bin/bash -c "$(brew install tig)"
